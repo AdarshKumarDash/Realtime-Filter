6e9d60961d32597604faaccf50f1ca615ad96260
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-04-13 19:31:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 nosex = "";
 nosey = "";
+clown_img = "";
+
+function preload() {
+    clown_img = loadImage("https://i.postimg.cc/VsS6L9Nk/clown-nose.png");
+}
 
 function setup() {
     canvas = createCanvas(640, 480);
@@ -25,6 +30,10 @@ function gotposes(results) {
 
 function draw() {
     image(video, 0, 0, 640, 480);
+    /*fill("#f34");
+    stroke("#333");
+    circle(nosex, nosey, 50);*/
+    image(clown_img, nosex - 20, nosey - 15, 40, 40);
 }
 
 function takesnapshot() {
